952dcfb564d8a7fdc28dde47e82d4162df6eb3e9
zc <2320092610@qq.com>
2021-06-06 23:01:02 +0800

    坦克类50%

---
+++ src/Utils/ImageUtils.java
@@ -0,0 +1,42 @@
+package utils;
+
+import tank.Tank;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ImageUtils {
+    private ImageUtils() {}
+
+    public static final Image TANK_U = new ImageIcon("image/tankU.gif").getImage();
+    public static final Image TANK_D = new ImageIcon("image/tankD.gif").getImage();
+    public static final Image TANK_L = new ImageIcon("image/tankL.gif").getImage();
+    public static final Image TANK_R = new ImageIcon("image/tankR.gif").getImage();
+
+    // 敌方坦克
+    public static final Image E_TANK_U = new ImageIcon("images/EtankU.png").getImage();
+    public static final Image E_TANK_D = new ImageIcon("images/EtankD.png").getImage();
+    public static final Image E_TANK_L = new ImageIcon("images/EtankL.png").getImage();
+    public static final Image E_TANK_R = new ImageIcon("images/EtankR.png").getImage();
+
+    // 难度1
+    public static final Image E1_TANK_U = new ImageIcon("images/E1tankU.png").getImage();
+    public static final Image E1_TANK_D = new ImageIcon("images/E1tankD.png").getImage();
+    public static final Image E1_TANK_L = new ImageIcon("images/E1tankL.png").getImage();
+    public static final Image E1_TANK_R = new ImageIcon("images/E1tankR.png").getImage();
+
+    // 难度2
+    public static final Image E2_TANK_U = new ImageIcon("images/E2tankU.png").getImage();
+    public static final Image E2_TANK_D = new ImageIcon("images/E2tankD.png").getImage();
+    public static final Image E2_TANK_L = new ImageIcon("images/E2tankL.png").getImage();
+    public static final Image E2_TANK_R = new ImageIcon("images/E2tankR.png").getImage();
+
+    // 难度3
+    public static final Image E3_TANK_U = new ImageIcon("images/E3tankU.png").getImage();
+    public static final Image E3_TANK_D = new ImageIcon("images/E3tankD.png").getImage();
+    public static final Image E3_TANK_L = new ImageIcon("images/E3tankL.png").getImage();
+    public static final Image E3_TANK_R = new ImageIcon("images/E3tankR.png").getImage();
+
+
+
+}
